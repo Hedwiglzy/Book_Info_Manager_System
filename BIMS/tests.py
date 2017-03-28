a2207090d450890a7c3dc277990c47f0ce8ea67c
Hedwiglzy <1432659378@qq.com>
2017-03-28 09:06:00 +0800

    20170328

--- BIMS/tests.py
+++ BIMS/tests.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-测试用
+测试
 """
-image = open('./static/image/avatar/hd/10001.jpg', 'wb')
-print(image.read())
-
+# image = open('./static/image/avatar/hd/10001.jpg', 'wb')
+# print(image.read())
+for i in range(10):
+    print('中文测试')
