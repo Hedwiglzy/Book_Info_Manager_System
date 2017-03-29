550c2d4369d243d813b5cbe2b0f24576cadf4f8b
Hedwiglzy <1432659378@qq.com>
2017-03-29 21:07:39 +0800

    20170329

--- BIMS/tests.py
+++ BIMS/tests.py
@@ -3,7 +3,45 @@
 """
 测试
 """
-# image = open('./static/image/avatar/hd/10001.jpg', 'wb')
-# print(image.read())
-for i in range(10):
-    print('中文测试')
+import pandas
+import pymysql
+import plotly
+from plotly.graph_objs import Scatter, Layout
+
+
+def connect_db():
+    """
+    连接数据库
+    """
+    connection = pymysql.connect(
+        host='127.0.0.1',
+        port=3306,
+        user='root',
+        password='qwer1234',
+        db='BIMS',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    return connection
+
+
+def select_table():
+    """
+    从表里面选择数据
+    """
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('select * from `bims_user`')
+    result = cursor.fetchall()
+    conn.commit()
+    conn.close()
+    return result
+
+
+RESULTS = select_table()
+INFO = pandas.DataFrame([result for result in RESULTS])
+print(INFO)
+plotly.offline.plot({
+    "data": [Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
+    "layout": Layout(title="hello world")
+})
