5a2b3577e2f8b251d7f64e9aa9ba041063eb9e4d
Hedwiglzy <1432659378@qq.com>
2017-03-30 20:57:42 +0800

    20170330

--- BIMS/tests.py
+++ BIMS/tests.py
@@ -3,45 +3,28 @@
 """
 测试
 """
-import pandas
-import pymysql
-import plotly
-from plotly.graph_objs import Scatter, Layout
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
 
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
 
-def connect_db():
-    """
-    连接数据库
-    """
-    connection = pymysql.connect(
-        host='127.0.0.1',
-        port=3306,
-        user='root',
-        password='qwer1234',
-        db='BIMS',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
-    return connection
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+import mpld3
 
-
-def select_table():
-    """
-    从表里面选择数据
-    """
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('select * from `bims_user`')
-    result = cursor.fetchall()
-    conn.commit()
-    conn.close()
-    return result
-
-
-RESULTS = select_table()
-INFO = pandas.DataFrame([result for result in RESULTS])
-print(INFO)
-plotly.offline.plot({
-    "data": [Scatter(x=[1, 2, 3, 4], y=[4, 3, 2, 1])],
-    "layout": Layout(title="hello world")
-})
+mpld3.enable_notebook()
+fig, ax = plt.subplots(subplot_kw=dict(axisbg='#EEEEEE'))
+ax.grid(color='white', linestyle='solid')
+N = 50
+scatter = ax.scatter(np.random.normal(size=N),
+                     np.random.normal(size=N),
+                     c=np.random.random(size=N),
+                     s=1000 * np.random.random(size=N),
+                     alpha=0.3,
+                     cmap=plt.cm.jet)
+ax.set_title("D3 Scatter Plot", size=18)
